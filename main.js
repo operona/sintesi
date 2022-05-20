29225ffd9115bb32b4a0a9e76dc21abfa087f3f6
operona <operona@elpuig.xeill.net>
2022-05-20 10:35:02 +0200

    sintesi

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ fetch("./cabezera.html")
   .then(data => {
     document.querySelector("cabezera").innerHTML = data;
   });
+
+
+  fetch("./footer.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("footer").innerHTML = data;
+    });
